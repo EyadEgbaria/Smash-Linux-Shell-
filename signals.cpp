babd30461c79e74f88cb643daa4a616e893f531e
EyadEgbaria <eyadegbaria9@gmail.com>
2025-01-25 02:25:39 +0200

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-    // TODO: Add your implementation
     cout << "smash: got ctrl-Z" << endl;
     SmallShell& smash = SmallShell::getInstance();
     pid_t curr_pid = smash.get_currPid();
@@ -21,7 +20,6 @@ void ctrlZHandler(int sig_num) {
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
     cout << "smash: got ctrl-C" << endl;
     SmallShell& smash = SmallShell::getInstance();
     pid_t curr_pid = smash.get_currPid();
